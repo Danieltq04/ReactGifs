d5e117be6cc7c316ecaf202ad1d6a56797c1d0a4
Danieltq04 <daniel.tq04@gmail.com>
2022-03-09 01:36:45 -0300

    inicio de useEffect

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const GifGrid = ({category}) => {
+    //useEffect para que no se renderice todo ante algún cambio
+    //Se renderiza pero de manera condicional
+    const [counter, setCounter] = useState(0)
+    useEffect( ()=>{
+        getGifs();
+    },[])
+    //El segundo parámetro es un arreglo de dependencias
+    //[] = no tiene dependencias, se ejecuta una única vez
     const getGifs = async() =>{
         const url = "https://api.giphy.com/v1/gifs/trending?q=Marvel&limit=10&api_key=q3FECLLySEElFd3y9fvrgQ0VU3RGHxVy";
         const resp = await fetch(url);
@@ -15,10 +23,12 @@ export const GifGrid = ({category}) => {
         
         console.log(gifs)
     }
-    getGifs();
+    //getGifs();
   return (
     <>
         <h3>{category}</h3>
+        <h1>{counter}</h1>
+        <button onClick={()=>{setCounter(counter+1)}}></button>
     </>
   )
 }
